5ddfa1f8a270c37ecb85b971acacb2c1d341381f
Sebastian Bordt <sbordt@posteo.de>
2024-09-23 18:39:01 +0200

    Update iterable_dataset.py

--- olmo/data/iterable_dataset.py
+++ olmo/data/iterable_dataset.py
@@ -73,7 +73,7 @@ class IterableDataset(torch.utils.data.IterableDataset[Dict[str, Any]]):
 
     def _build_and_save_global_indices(self):
         assert self.work_dir is not None
-        #self.global_indices_file = Path(self.work_dir) / "global_indices.npy"
+        self.global_indices_file = Path(self.work_dir) / "global_indices.npy"
         if self.fs_local_rank == 0:
             log.info("Saving global data order indices...")
             self.global_indices_file.parent.mkdir(parents=True, exist_ok=True)
@@ -85,7 +85,7 @@ class IterableDataset(torch.utils.data.IterableDataset[Dict[str, Any]]):
             global_indices_mmap.flush()
             del global_indices_mmap
             log.info("Global data order indices saved to '%s'", self.global_indices_file)
-            #print("Did not generate the global indices, instead relying on the pre-existing file")
+            # print("Did not generate the global indices, instead relying on the pre-existing file")
         barrier()
 
     def _build_global_indices(self) -> np.ndarray:
