4de63e3fc0cd82a1f6b3554c02d1845b5b0bf785
Sebastian Bordt <sbordt@posteo.de>
2024-09-27 13:44:18 +0200

    Update download_checkpoint.py

--- train-on-test/download_checkpoint.py
+++ train-on-test/download_checkpoint.py
@@ -17,7 +17,7 @@ def download_file(url, directory, filename):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--checkpoint_url", default="https://olmo-checkpoints.org/ai2-llm/olmo-medium/6qvoqf3c/step300000-unsharded/")
+    parser.add_argument("--checkpoint_url", default="https://olmo-checkpoints.org/ai2-llm/olmo-medium/6qvoqf3c/step300000-unsharded")
     parser.add_argument("--output_dir", default="/mnt/qb/luxburg/sbordt10/OLMo-7B-checkpoints/step300000-unsharded")
     args = parser.parse_args()
 
