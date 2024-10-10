8d2bcceb3e67df247fa14a58120ab07a1fd7a0b1
Sebastian Bordt <sbordt@posteo.de>
2024-10-10 05:07:01 +0200

    Update train_olmo.sh

--- train-on-test/scripts/train_olmo.sh
+++ train-on-test/scripts/train_olmo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=2-00:00:00  # Runtime in D-HH:MM:SS    
+#SBATCH --time=3-00:00:00  # Runtime in D-HH:MM:SS    
 #SBATCH --output=/mnt/qb/work/luxburg/sbordt10/logs/olmo/%j.out  
 #SBATCH --error=/mnt/qb/work/luxburg/sbordt10/logs/olmo/%j.err   
 #SBATCH --open-mode=append
@@ -7,7 +7,7 @@
 #SBATCH --partition=a100-galvani 
 #SBATCH --nodes=1  
 #SBATCH --ntasks=1       
-#SBATCH --gres=gpu:2              
+#SBATCH --gres=gpu:8              
 
 scontrol show job ${SLURM_JOB_ID}
 nvidia-smi
@@ -17,4 +17,4 @@ export WANDB__SERVICE_WAIT=6000
 cd $WORK/OLMo
 source activate olmo-3.11
 
-torchrun --nproc_per_node=2 --master_port=29501 scripts/train.py configs/official/OLMo-1B.yaml --load_path="/mnt/qb/luxburg/sbordt10/OLMo-1B/step369040-unsharded/"
+torchrun --nproc_per_node=8 --master_port=29501 scripts/train.py configs/official/OLMo-7B.yaml --load_path="/mnt/qb/luxburg/sbordt10/OLMo-7B-checkpoints/step300000-unsharded/"
