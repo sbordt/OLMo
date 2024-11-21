ea8f50f632dc2390f67889c510dc155494511e52
Sebastian Bordt <sbordt@posteo.de>
2024-11-21 10:17:46 +0100

    Update train_olmo_7B_ferranit.sh

--- train-on-test/scripts/train_olmo_7B_ferranit.sh
+++ train-on-test/scripts/train_olmo_7B_ferranit.sh
@@ -19,4 +19,4 @@ export WANDB__SERVICE_WAIT=6000
 cd /weka/luxburg/sbordt10/OLMo
 source activate olmo-3.11
 
-torchrun --nproc_per_node=4 --master_port=29501 scripts/train.py configs/official/OLMo-7B-step300080-unsharded.yaml --load_path="/weka/luxburg/sbordt10/v1_5-mix-mitch-ish/step302950-unsharded"
+torchrun --nproc_per_node=4 --master_port=29501 scripts/train.py configs/official/OLMo-7B-step300080-unsharded.yaml --load_path="/weka/luxburg/sbordt10/v1_5-mix-mitch-ish/latest-unsharded"
