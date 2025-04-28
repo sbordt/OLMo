4f3ae58ee5fb803eab48b5fdb9ada1ee31ca6494
Sebastian Bordt <sbordt@posteo.de>
2025-04-28 19:00:53 +0200

    Create vllm-example.py

---
+++ single-training-run/evals/vllm-example.py
@@ -0,0 +1,30 @@
+from vllm import LLM, SamplingParams
+
+prompts = [
+    "Hello, my name is",
+    "The president of the United States is",
+    "The capital of France is",
+    "The future of AI is",
+]
+sampling_params = SamplingParams(
+    temperature=0.0,          # deterministic demo
+    max_tokens=20,
+    logprobs=1,               # generated tokens
+    prompt_logprobs=1         # prefix tokens
+)
+
+
+
+
+llm = LLM(model="allenai/OLMo-1B-hf")
+
+
+
+
+
+outputs = llm.generate(prompts, sampling_params)
+
+for output in outputs:
+    prompt = output.prompt
+    generated_text = output.outputs[0].text
+    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
