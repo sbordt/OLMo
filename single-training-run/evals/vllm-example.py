10fb688278602df67dc4f429e7441cdaf2165351
Sebastian Bordt <sbordt@posteo.de>
2025-04-28 22:59:14 +0200

    Update vllm-example.py

--- single-training-run/evals/vllm-example.py
+++ single-training-run/evals/vllm-example.py
@@ -8,7 +8,7 @@ prompts = [
 ]
 sampling_params = SamplingParams(
     temperature=0.0,          # deterministic demo
-    max_tokens=20,
+    max_tokens=1,             # generate a single dummy token
     logprobs=0,               # generated tokens
     prompt_logprobs=0         # prefix tokens
 )
@@ -16,10 +16,15 @@ sampling_params = SamplingParams(
 
 # load hellaswag queries
 import pickle
-with open("hellaswag_queries.pkl", "rb") as f:
-    prompts = pickle.load(f)
+#with open("hellaswag_queries.pkl", "rb") as f:
+#    prompts = pickle.load(f)
 
-llm = LLM(model="allenai/OLMo-1B-hf", download_dir = "/mnt/lustre/work/luxburg/sbordt10/.cache/vllm", enable_prefix_caching=False)
+import os
+download_dir = None
+if os.path.exists("/mnt/lustre/work"):
+    download_dir = "/mnt/lustre/work/luxburg/sbordt10/.cache/vllm"
+
+llm = LLM(model="allenai/OLMo-1B-hf", download_dir = download_dir, enable_prefix_caching=False)
 
 
 outputs = llm.generate(prompts, sampling_params)
@@ -28,6 +33,10 @@ for output in outputs:
     prompt = output.prompt
     generated_text = output.outputs[0].text
     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+# if the "../results" directory does not exist, create it
+if not os.path.exists("../results"):
+    os.makedirs("../results")
 
 # save the outputs
 with open("../results/hellaswag_outputs.pkl", "wb") as f:
