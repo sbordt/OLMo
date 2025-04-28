75060167ae8112dde4a23c36b2d63fa694566d8a
Sebastian Bordt <sbordt@posteo.de>
2025-04-28 16:32:14 +0200

    scripts

---
+++ single-training-run/scripts/train_4xH100.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=0-00:30:00  # Runtime in D-HH:MM:SS    
+#SBATCH --output=/weka/luxburg/sbordt10/logs/single-training-run/%j.out  
+#SBATCH --error=/weka/luxburg/sbordt10/logs/single-training-run/%j.err   
+#SBATCH --open-mode=append
+#SBATCH --job-name=olmo  
+#SBATCH --partition=h100-ferranti
+#SBATCH --nodes=1  
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64    
+#SBATCH --mem=500G   
+#SBATCH --gres=gpu:4              
+
+scontrol show job ${SLURM_JOB_ID}
+nvidia-smi
+export NCCL_TIMEOUT=1800000
+export WANDB__SERVICE_WAIT=6000
+
+cd /weka/luxburg/sbordt10/OLMo
+source activate olmo-3.11
+
+torchrun --nproc_per_node=4 --master_port=29501 scripts/train.py single-training-run/scripts/OLMo-1B.yaml 
